adb6dd4ff4a3c419e730c43c66ccf32c3b6509ea
EjoneAnalumi <ejoneanalumi@gmail.com>
2025-05-18 14:38:33 +0200

    krijimi i MainApp
    faqja ku ekzekutohet kodi (lidhja me views)

--- src/main/java/app/MainApp.java
+++ src/main/java/app/MainApp.java
@@ -1,4 +1,21 @@
 package app;
 
-public class MainApp {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/login.fxml"));
+        primaryStage.setTitle("Password Manager");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
