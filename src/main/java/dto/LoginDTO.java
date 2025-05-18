78f39780fe160ae3366ea07a1359834544e56419
EjoneAnalumi <ejoneanalumi@gmail.com>
2025-05-18 14:39:52 +0200

    krijimi i LoginDTO

--- src/main/java/dto/LoginDTO.java
+++ src/main/java/dto/LoginDTO.java
@@ -1,4 +1,15 @@
 package dto;
 
 public class LoginDTO {
+    private int userId;
+    private String username;
+    private String encryptionKey;
+
+    // Getters dhe Setters
+    public int getUserId() { return userId; }
+    public void setUserId(int userId) { this.userId = userId; }
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+    public String getEncryptionKey() { return encryptionKey; }
+    public void setEncryptionKey(String encryptionKey) { this.encryptionKey = encryptionKey; }
 }
